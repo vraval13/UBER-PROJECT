73ac4b0bc925b041bd1b98eb29cee06798e4b1b3
Vyom Raval <ravalvyom17@gmail.com>
2024-12-13 15:11:24 +0530

    captain creation - 2

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -12,6 +12,13 @@ module.exports.registerUser = async (req, res, next) => {
 
   const { fullname, email, password } = req.body;
 
+  const isUserAlreadyExist = await userModel.findOne({ email });
+
+  if (isUserAlreadyExist) 
+  {
+    return res.status(400).json({ message: 'User already exist' });
+  }
+
   const hashedPassword = await userModel.hashPassword(password);
 
   const user = await userService.createUser({
--- Backend/routes/captain.routes.js
+++ Backend/routes/captain.routes.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const {body} = require("express-validator");
+const captainController = require("../controllers/captain.controller");
+
 
 router.post('/register',[
     body('email').isEmail().withMessage('Invalid Email'),
@@ -11,7 +13,7 @@ router.post('/register',[
     body('vehicle.capacity').isInt({ min: 1 }).withMessage('Capacity must be at least 1'),
     body('vehicle.vehicleType').isIn([ 'car', 'motorcycle', 'auto' ]).withMessage('Invalid vehicle type')
 ],
-
+  captainController.registerCaptain
 )
 
 
