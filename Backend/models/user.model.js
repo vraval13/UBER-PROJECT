c3c9f230338f2cf7145aae518f92c7be259152d3
Vyom Raval <ravalvyom17@gmail.com>
2024-12-14 00:10:47 +0530

    some minor mistake fixed

--- Backend/models/user.model.js
+++ Backend/models/user.model.js
@@ -42,7 +42,7 @@ userSchema.methods.comparePassword = async function(password){
 }
 
 userSchema.statics.hashPassword = async function(password){
-  return await bcrpt.hash(password,10);
+  return await bcrypt.hash(password,10);
 }
 
 const userModel = mongoose.model('user',userSchema);
