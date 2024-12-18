c5b9d7e1a282044aac75cf6c607644c8b937d59c
Vyom Raval <ravalvyom17@gmail.com>
2024-12-18 19:08:29 +0530

    socket - I

---
+++ Backend/socket.js
@@ -0,0 +1,43 @@
+const socketIo = require('socket.io');
+const userModel = require("./models/user.model");
+const captainModel = require("./models/captain.model");
+
+function initializeSocket(server) {
+  io = socketIo(server, {
+    cors: {
+      origin: '*',
+      methods: ['GET', 'POST']
+    }
+  });
+
+  io.on('connection', (socket) => {
+    console.log(`Client connected: ${socket.id}`);
+
+    socket.on('join', async (data) => {
+      const { userId, userType } = data;
+
+      console.log(`User ${userId} joined as ${userType}`);
+
+      if (userType === 'user') {
+        await userModel.findByIdAndUpdate(userId, { socketId: socket.id });
+      } else if (userType === 'captain') {
+        await captainModel.findByIdAndUpdate(userId, { socketId: socket.id });
+      }
+    });
+
+    socket.on('disconnect', () => {
+      console.log(`Client disconnected : ${socket.id}`);
+    });
+  });
+}
+
+function sendMessageToSocketId(socketId, message) {
+  if (io) {
+    io.to(socketId).emit('message', message);
+  }
+  else {
+    console.log('Socket.io not initialized');
+  }
+}
+
+module.exports = { initializeSocket, sendMessageToSocketId };
