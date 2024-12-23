dd59045f1713915c12eb423dd3fc66cbb9857ecc
Vyom Raval <ravalvyom17@gmail.com>
2024-12-23 20:57:53 +0530

    made changes

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  distDir: 'dist', // Set the output directory to 'dist' instead of '.next'
+  distDir: '.next', // Set the output directory to 'dist' instead of '.next'
 };
 
 export default nextConfig;
